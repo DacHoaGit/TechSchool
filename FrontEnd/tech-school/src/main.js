2568d14bda8ce3d7bfa3ad474fcbbe031c5b8e50
Lê Đắc Hòa <Ledachoa99@gmail.com>
2022-11-20 21:24:59 +0700

    update

--- FrontEnd/tech-school/src/main.js
+++ FrontEnd/tech-school/src/main.js
@@ -1,5 +1,27 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+import router from './router'
+import 'ant-design-vue/dist/antd.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import 'bootstrap/dist/css/bootstrap-utilities.min.css';
+import { Button,Drawer, List, Menu, Card, Table, Avatar } from 'ant-design-vue';
+import axios from 'axios';
+window.axios = axios;
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+
+app.use(pinia)
+app.use(router)
+app.use(Button)
+app.use(Drawer)
+app.use(List)
+app.use(Card)
+app.use(Avatar)
+app.use(Table)
+app.use(Menu)
+
+app.mount('#app')
